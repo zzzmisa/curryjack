ae87094d491104b5da9cef6c84b2fb7cde612f5f
zzzmisa <zzzmisa.office@gmail.com>
2019-02-20 22:30:56 +0900

    CjkHeroを編集

---
+++ src/components/organsms/CjkHero.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="hero">
+    <h1 class="title">
+      <img alt="Vue logo" src="@/assets/logo.png">
+    </h1>
+    <h2 class="subtitle">2,100円に近づけろ！超えたらアウト！
+      <b-icon pack="fa" icon="question-circle" type="is-primary"></b-icon>
+    </h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CjkHero',
+}
+</script>
+
+<style scoped>
+@import url(https://use.fontawesome.com/releases/v5.2.0/css/all.css);
+</style>
--- src/components/templates/CjkFirstView.vue
+++ src/components/templates/CjkFirstView.vue
@@ -1,3 +1,19 @@
 <template>
-  <p>ファーストビュー</p>
+  <CjkHero />
 </template>
+
+<script>
+// @ is an alias to /src
+import CjkHero from "@/components/organsms/CjkHero.vue";
+
+export default {
+  name: "CjkFirstView",
+  components: {
+    CjkHero
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
