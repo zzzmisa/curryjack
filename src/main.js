3fc263fcca621f18f5e9250edc61f8cc33857df1
zzzmisa <zzzmisa.office@gmail.com>
2019-02-20 10:07:07 +0900

    Buefyをインストール

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import Buefy from 'buefy' //buefy
+import 'buefy/dist/buefy.css' //buefy
+
+Vue.use(Buefy) //buefy
+
 Vue.config.productionTip = false
 
 new Vue({
