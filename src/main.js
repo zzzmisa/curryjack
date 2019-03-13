344cc06e24d952d39d298b2a0045d0f488a0d5f6
zzzmisa <zzzmisa.office@gmail.com>
2019-03-13 09:54:28 +0900

    vue-analyticsを追加

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import VueAnalytics from 'vue-analytics' // vue-analytics
 import Buefy from 'buefy' // buefy
-// import 'buefy/dist/buefy.css' //buefy
+// import 'buefy/dist/buefy.css' //カスタマイズしている場合はコメントアウト
 
 Vue.use(Buefy) // buefy
+
+// vue-analytics
+Vue.use(VueAnalytics, {
+  id: 'UA-88635038-4',
+  router
+})
 
 Vue.config.productionTip = false
 
