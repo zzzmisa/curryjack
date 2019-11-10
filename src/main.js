a89529401c29444dff75387a7c156558b0f96646
zzzmisa <zzzmisa.office@gmail.com>
2019-11-10 14:20:21 +0900

    Add vue-i18n

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import VueAnalytics from 'vue-analytics' // vue-analytics
 import Buefy from 'buefy' // buefy
+import i18n from './i18n'
 // import 'buefy/dist/buefy.css' //カスタマイズしている場合はコメントアウト
 
 Vue.use(Buefy) // buefy
@@ -17,5 +18,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
