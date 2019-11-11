c67419fe499e70af8868a784cb51327f1e3e3095
zzzmisa <zzzmisa.office@gmail.com>
2019-11-11 23:11:56 +0900

    Add a subdirectory for en

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   pluginOptions: {
     i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
+      locale: 'ja',
+      fallbackLocale: 'ja',
       localeDir: 'locales',
       enableInSFC: true
     }
