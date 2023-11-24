ccea441c0c4c10fdecf1297efe221c21a43d9070
zzzmisa <zzzmisa.office@gmail.com>
2023-11-24 12:13:58 +0900

    Add GTM instead of GA

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueAnalytics from 'vue-analytics' // vue-analytics
+import VueGtm from '@gtm-support/vue2-gtm'
 import Buefy from 'buefy' // buefy
 import i18n from './i18n'
 // import 'buefy/dist/buefy.css' //カスタマイズしている場合はコメントアウト
 
-Vue.use(Buefy) // buefy
-
-// vue-analytics
-Vue.use(VueAnalytics, {
-  id: 'UA-88635038-4',
-  router
+Vue.use(VueGtm, {
+  id: 'GTM-M5SJDZ36'
 })
+
+Vue.use(Buefy) // buefy
 
 Vue.config.productionTip = false
 
